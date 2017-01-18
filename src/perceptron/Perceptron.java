e3c83ca6534216f488a039079af2a3e660690b31
Matt Crowder <mcrowder65@gmail.com>
2017-01-18 14:04:16 -0700

    first commit

---
+++ src/perceptron/Perceptron.java
@@ -0,0 +1,20 @@
+package perceptron;
+
+import toolkit.Matrix;
+import toolkit.SupervisedLearner;
+
+public class Perceptron extends SupervisedLearner {
+
+	@Override
+	public void train(Matrix features, Matrix labels) throws Exception {
+		// TODO Auto-generated method stub	
+		
+	}
+
+	@Override
+	public void predict(double[] features, double[] labels) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
