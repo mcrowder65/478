7482c7dd1ab8e788681665282a3d94b11e088f57
Matt Crowder <mcrowder65@gmail.com>
2017-01-26 20:46:06 -0700

    sync?

--- src/perceptron/Perceptron.java
+++ src/perceptron/Perceptron.java
@@ -7,14 +7,15 @@ public class Perceptron extends SupervisedLearner {
 
 	@Override
 	public void train(Matrix features, Matrix labels) throws Exception {
-		// TODO Auto-generated method stub	
-		
+		// TODO Auto-generated method stub
+		System.out.println("train!");
 	}
 
 	@Override
 	public void predict(double[] features, double[] labels) throws Exception {
 		// TODO Auto-generated method stub
-		
+		System.out.println("predict!");
+
 	}
 
 }
