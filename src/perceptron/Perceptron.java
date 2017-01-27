b260d95d9819ddfd52304d63237440abdfa32dfb
Matt Crowder <mcrowder65@gmail.com>
2017-01-27 08:37:08 -0700

    added some methods, got perceptron to run

--- src/perceptron/Perceptron.java
+++ src/perceptron/Perceptron.java
@@ -1,9 +1,17 @@
 package perceptron;
 
+import java.util.Random;
+
 import toolkit.Matrix;
 import toolkit.SupervisedLearner;
+import utilities.Utilities;
 
 public class Perceptron extends SupervisedLearner {
+	private Random rand;
+
+	public Perceptron(Random rand) {
+		this.rand = rand;
+	}
 
 	@Override
 	public void train(Matrix features, Matrix labels) throws Exception {
@@ -14,8 +22,7 @@ public class Perceptron extends SupervisedLearner {
 	@Override
 	public void predict(double[] features, double[] labels) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("predict!");
-
+		Utilities.outputArray("features", features);
 	}
 
 }
--- src/utilities/Utilities.java
+++ src/utilities/Utilities.java
@@ -1,4 +1,31 @@
 package utilities;
 
 public class Utilities {
+	static public void outputArray(double[] arr) {
+		System.out.print("[ ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(i != arr.length - 1 ? arr[i] + ", " : arr[i]);
+		}
+		System.out.print(" ]");
+		System.out.println();
+	}
+
+	static public void outputArray(String[] arr) {
+		System.out.print("[ ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(i != arr.length - 1 ? arr[i] + ", " : arr[i]);
+		}
+		System.out.print(" ]");
+		System.out.println();
+	}
+
+	static public void outputArray(String prepend, double[] arr) {
+		System.out.print(prepend + " ");
+		outputArray(arr);
+	}
+
+	static public void outputArray(String prepend, String[] arr) {
+		System.out.print(prepend + " ");
+		outputArray(arr);
+	}
 }
