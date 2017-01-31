e3f170bb1f7c69b406ac5dc002102e15a83dc2df
Matt Crowder <mcrowder65@gmail.com>
2017-01-31 07:54:04 -0700

    slowly adding more functionality to multiple perceptrons

--- src/perceptron/MultiplePerceptron.java
+++ src/perceptron/MultiplePerceptron.java
@@ -1,5 +1,7 @@
 package perceptron;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import toolkit.Matrix;
@@ -10,19 +12,28 @@ public class MultiplePerceptron extends Perceptron {
 
 	private final static double THRESHOLD = 0;
 
-	private double[] myWeights;
 	private final static int MAX_ITERATIONS = 5;
 
 	private final static double LEARNING_RATE = 0.1;
+	private List<Matrix> matrices;
+	private List<double[]> weights;
 
 	public MultiplePerceptron(Random rand) {
 		this.rand = rand;
+		matrices = new ArrayList<>();
+		weights = new ArrayList<>();
 	}
 
 	@Override
 	public void train(Matrix features, Matrix labels) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("multiple perceptron train!");
+		// should always be 3 for our purposes.
+		int size = labels.m_enum_to_str.get(0).size();
+
+		for (int i = 0; i < size; i++) {
+			System.out.println(i);
+		}
 
 	}
 
