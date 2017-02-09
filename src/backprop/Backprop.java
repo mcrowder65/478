b8660ae2804656916ec5802d1477bac3da33fbac
Matt Crowder <mcrowder65@gmail.com>
2017-02-09 14:23:18 -0700

    doing some backprop practicing on the homework

--- src/backprop/Backprop.java
+++ src/backprop/Backprop.java
@@ -10,18 +10,26 @@ public class Backprop extends SupervisedLearner {
 	private Random rand;
 	private double[] myWeights;
 
+	final private static int BIAS = 1;
+
 	public Backprop(Random rand) {
 		this.rand = rand;
 	}
 
-	// TODO figure out parameters
-	private double calculateNet(double[] feature) {
-		return -1;
+	private double calculateNet(double[] input, int iteration) {
+		// Sigma WiXi
+		// + 1 to account for bias.
+		double net = 0;
+		for (int i = 0; i < input.length + 1; i++) {
+			net += (this.myWeights[i + iteration] * (i < input.length ? input[i] : BIAS));
+		}
+		return net;
 	}
 
 	// TODO figure out parameters
-	private double calculateOutput() {
-		return -1;
+	private double calculateOutput(double net) {
+		double output = 1 / (1 + Math.exp(-net));
+		return output;
 	}
 
 	// TODO figure out parameters
@@ -33,12 +41,29 @@ public class Backprop extends SupervisedLearner {
 	public void train(Matrix features, Matrix labels) throws Exception {
 		// TODO calculate weight size based on hidden nodes
 		// FIXME temp
-		this.myWeights = new double[6];
+		double[] inputs = new double[2];
+		inputs[0] = 0;
+		inputs[1] = 0;
 
-		// Utilities.outputArrayList(features.m_data);
-		this.myWeights = Utilities.initializeWeights(this.myWeights, this.rand, 0, 0);
-		Utilities.outputArray(this.myWeights, true);
+		// TODO this is supposed to be twice the inputs
+		int numHiddenNodes = 2;
 
+		int weightLength = (inputs.length + 1) * (numHiddenNodes + 1);
+		this.myWeights = new double[weightLength];
+		this.myWeights = Utilities.initializeWeights(this.myWeights, this.rand, 1, 1);
+		double[] netArray = new double[numHiddenNodes + 1];
+		int iteration = 0;
+		for (int i = 0; i < netArray.length - 1; i++) {
+			netArray[i] = calculateNet(inputs, iteration++);
+		}
+		double[] outputArray = new double[numHiddenNodes + 1];
+		for (int i = 0; i < outputArray.length - 1; i++) {
+			outputArray[i] = calculateOutput(netArray[i]);
+		}
+		netArray[iteration] = calculateNet(outputArray, iteration);
+		outputArray[iteration] = calculateOutput(netArray[iteration]);
+		// TODO calculate delta array
+		int x = 0;
 	}
 
 	@Override
