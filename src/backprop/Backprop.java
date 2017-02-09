c7f08cf8ddb4f7cf7de9d95e73c4444c266eced3
Matt Crowder <mcrowder65@gmail.com>
2017-02-09 11:20:03 -0700

    added backdrop

---
+++ src/backprop/Backprop.java
@@ -0,0 +1,27 @@
+package backprop;
+
+import java.util.Random;
+
+import toolkit.Matrix;
+import toolkit.SupervisedLearner;
+
+public class Backprop extends SupervisedLearner {
+	private Random rand;
+
+	public Backprop(Random rand) {
+		this.rand = rand;
+	}
+
+	@Override
+	public void train(Matrix features, Matrix labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void predict(double[] features, double[] labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
